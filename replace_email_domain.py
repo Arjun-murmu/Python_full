afcccca2a0ce54b9978a1960c8928f1a35bca992
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2024-08-30 08:48:15 +0530

    Create replace_email_domain.py

---
+++ replace_email_domain.py
@@ -0,0 +1,21 @@
+#Replace Email domain
+# Example usage:
+def replace_domain(email, old_domain, new_domain):
+    if "@" + old_domain in email:
+        index = email.index("@" + old_domain)
+        new_email = email[:index] + "@" + new_domain
+        return new_email
+    return email
+print(replace_domain("user@gmail.com", "gmail.com", "example.com"))
+
+#Example 
+def replace_domain(email, old_domain, new_domain):
+  if "@" + old_domain in email:
+    index = email.index("@" + old_domain)
+    new_email = email[:index] + "@" + new_domain
+    return new_email
+  return email
+  
+print(replace_domain("arjun@gmail.com","gmail.com","dog,com"))
+
+
