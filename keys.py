620e055785e6a14524820ab12da48853133d3c23
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2024-09-29 23:19:47 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -10,20 +10,24 @@ print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "
 
 #Q2
 def groups_per_user(group_dictionary):
-	user_groups = {}
-	# Go through group_dictionary
-	for group,users in group_dictionary.items():
-		# Now go through the users in the group
-		for user in users:
-			# Now add the group to the the list of
-			if user not in user_groups:
-				user_groups[user] = []
-				user_groups[user].append(group)
-# groups for this user, creating the entry
-# in the dictionary if necessary
+    user_groups = {}
+    
+    # Go through group_dictionary
+    for group, users in group_dictionary.items():
+        # Now go through the users in the group
+        for user in users:
+            # Create the entry for the user if it doesn't exist
+            if user not in user_groups:
+                user_groups[user] = []
+            # Add the group to the user's list of groups
+            user_groups[user].append(group)
 
-	return(user_groups)
+    return user_groups
 
-print(groups_per_user({"local": ["admin", "userA"],
-		"public":  ["admin", "userB"],
-		"administrator": ["admin"] }))
+# Example usage
+print(groups_per_user({
+    "local": ["admin", "userA"],
+    "public": ["admin", "userB"],
+    "administrator": ["admin"]
+}))
+# Output : {'admin': ['local', 'public', 'administrator'], 'userA': ['local'], 'userB': ['public']}
