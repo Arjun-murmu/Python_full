4af0659748719fc75222053d37690aa8d8cff8d4
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2024-10-01 14:30:36 +0530

    Create Method.py

---
+++ Method.py
@@ -0,0 +1,10 @@
+class N_name:
+    nam = "Jaga"
+    def spe(self):
+        print("My Name is {}".format(self.nam))
+
+        
+chan = N_name()
+chan.nam = "Arjun Murmu"
+chan.spe()
+My Name is Arjun Murmu
